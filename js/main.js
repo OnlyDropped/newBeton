7742c7878d0f93ed74b4a8c36c2a9a3033a1684a
OnlyDropped <asheburenkov@yandex.ru>
2021-08-10 15:30:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function () {
+
+  $(".star").rateYo({
+    rating: 5,
+    starWidth: "16px",
+    normalFill: "#ececec",
+    ratedFill: "#ffb400"
+  });
+  //  $('.header__select').styler();
+  //  $('.footer__content-select').styler();
+
+  //  $('.center').slick({
+  //    centerMode: true,
+  //    slidesToShow: 1,
+  //    autoplay: true,
+  //    autoplaySpeed: 6000,
+  //    dots: true,
+  //    arrows: true,
+  //    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.//svg" alt=""></button>',
+  //    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.//svg" alt=""></button>',
+  //    //infinite: true,
+  //    //speed: 300,
+  //    centerPadding: '100px',
+  //    responsive: [
+  //      {
+  //        breakpoint: 375,
+  //        settings: {
+  //          arrows: false,
+  //          dots: false,
+  //        }
+  //      }
+  //    ]
+  //  });
+
+
+
+
+});
